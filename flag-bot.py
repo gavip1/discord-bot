26af896da415ea3d8ab79d25a0d12df6fbc2f527
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2023-07-16 00:24:51 -0700

    added a country list and changed the system a bit
    country_list.csv might be used for the emote id later. For now, I'm using dictionary that only has 2 flags which can later be changed so that it picks any country in the country list

--- flag-bot.py
+++ flag-bot.py
@@ -2,6 +2,7 @@ import discord
 import random
 import asyncio
 import os
+import pandas
 from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,8 +10,10 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
 timeout_delta = timedelta(seconds=15)
+
+df = pandas.read_csv("country_list.csv")
+
 
 @client.event
 async def on_ready():
@@ -23,36 +26,27 @@ async def on_message(message):
 
     if message.content == "!flag":
         mention = message.author.mention
-        choices = ["indonesia", "Indonesia", "Polish", "polish", "poland", "Poland"]
-        flag_pl = ["Polish", "polish", "poland", "Poland"]
-        flag_id = ["indonesia", "Indonesia", "indonesian", "Indonesian"]
+        flags = {1: ["Poland", "Polish", "_pl:"], 2: ["Indonesia", "Indonesian", "_id:"]}
 
-        answer = random.choice(['Poland', 'Indonesia'])
-        await message.channel.send(f"{mention} is this Poland or Indonesia (10s)")
-        if answer == 'Poland':  #send the flag as an emote based of the random choice
-            await message.channel.send(':flag_pl:')
-            answer_flag = flag_pl
-            not_answer = flag_id
-        else:
-            await message.channel.send(':flag_id:')
-            answer_flag = flag_id
-            not_answer = flag_pl
+        pick = random.randint(1, len(flags))
+        await message.channel.send(f"{mention} what flag is this?")
+        await message.channel.send(":flag" + flags[pick][2])
 
         def is_correct(m):  #ensures the answer is from op and it is a valid answers within the choices
-            return m.author == message.author and m.content in choices
+            return m.author == message.author and m.content.capitalize() in df.values
 
         try:
             guess = await client.wait_for('message', check=is_correct, timeout=10.0)
         except asyncio.TimeoutError:
             await message.author.timeout(timeout_delta, reason="too stupid")
-            return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+            return await message.channel.send(f'Sorry, you took too long it was {flags[pick][0]}.')
         
         if random.randint(1,10) == 5:
             await message.channel.send(":wtf:")
-        if str(guess.content) in answer_flag:
+        if str(guess.content).capitalize() in flags[pick]:
             await message.channel.send('You are right!')
-        elif str(guess.content) in not_answer:
-            await message.channel.send(f'Oops. It is actually {answer}.')
+        else:
+            await message.channel.send(f'Oops. It is actually {flags[pick][0]}.')
             await message.author.timeout(timeout_delta, reason="too stupid")
 
 token = os.environ.get('TOKEN')   
